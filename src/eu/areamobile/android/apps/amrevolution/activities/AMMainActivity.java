546634f802d4d5d43631c9ee3fac964323c0176b
Federex81 <fedantonini81@gmail.com>
2012-11-18 21:46:18 +0100

    Federico: commit 1.

--- src/eu/areamobile/android/apps/amrevolution/activities/AMMainActivity.java
+++ src/eu/areamobile/android/apps/amrevolution/activities/AMMainActivity.java
@@ -49,9 +49,6 @@ public class AMMainActivity extends AMBaseActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.menu_settings:{
-			Log.d(TAG, "on settings item clicked");
-		}break;
 		case R.id.news_activity:{
 			Intent activityIntent = new Intent(this, AMNewsActivity.class);
 			startActivity(activityIntent);
